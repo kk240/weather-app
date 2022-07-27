2c66c7d60949d81692a28505a97740012a08f974
Kundan Suthar <kundanss22@gmail.com>
2022-07-27 00:24:29 -0700

    initialise

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "3ce11fed0dmsh76aa80e0b07aa82p15f663jsn367ba64fc06e",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "6b86abbc501487f8e5c36f9ac5acf7c2"; 
